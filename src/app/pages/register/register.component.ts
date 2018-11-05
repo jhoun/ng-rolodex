426753f53b51055ed73d46212fda9318caee793c
jhoun <jayhoun@gmail.com>
2018-11-05 12:31:25 -1000

    added data validation to register page

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,15 +1,64 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-register',
+  selector: 'register-page',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
 
+  registerFormData: {
+    username: string;
+    password: string;
+    name: string;
+    email: string;
+    address: string;
+  } = {
+    username: '',
+    password: '',
+    name: '',
+    email: '',
+    address: ''
+  }
+
+  validNewUserData: boolean = false;
+
   constructor() { }
 
+  login() {
+    console.log('hello');
+  }
+
+
   ngOnInit() {
+  }
+
+  validateNewUserData(input) {
+    if (!this.registerFormData[input]) {
+      this.validNewUserData = false;
+    }
+    else if (this.registerFormData[input].length < 3) {
+      this.validNewUserData = false;
+    }
+    else {
+      this.validNewUserData = true;
+    }
+  }
+
+  // validateEmail() {
+  //   if (!this.registerFormData.email) {
+  //     this.validName = false;
+  //   }
+  //   else if (this.registerFormData.username.length < 3) {
+  //     this.validName = false;
+  //   }
+  //   else {
+  //     this.validName = true;
+  //   }
+  // }
+
+   isDisabled(){
+    return !this.validNewUserData;
   }
 
 }
